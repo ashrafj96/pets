cafb10fb10878ade17253a02504a6b5e3384e95c
fares shomali <farisshomali@gmail.com>
2020-09-16 11:40:28 +0300

    rescue done and fix the android x mainifist

--- lib/pages/rescue.dart
+++ lib/pages/rescue.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:pets/widgets/camera_screen.dart';
 
 class Rescue extends StatefulWidget {
@@ -8,7 +9,7 @@ class Rescue extends StatefulWidget {
   _RescueState createState() => _RescueState();
 }
 
-class _RescueState extends State<Rescue> with TickerProviderStateMixin {
+class _RescueState extends State<Rescue> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +30,66 @@ class _RescueState extends State<Rescue> with TickerProviderStateMixin {
           ),
         ),
       ),
-      body: CameraScreen(),
+      body: Center(
+        child: CameraImage(),
+      ),
     );
+  }
+}
+
+class CameraImage extends StatefulWidget {
+  @override
+  MyImagePickerState createState() => MyImagePickerState();
+}
+
+class MyImagePickerState extends State {
+  File imageURI;
+
+  Future getImageFromCamera() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      imageURI = image;
+    });
+  }
+
+  Future getImageFromGallery() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      imageURI = image;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+          imageURI == null
+              ? Text('No image selected.')
+              : Image.file(imageURI,
+                  width: 300, height: 200, fit: BoxFit.cover),
+          Container(
+              margin: EdgeInsets.fromLTRB(0, 30, 0, 20),
+              child: RaisedButton(
+                onPressed: () => getImageFromCamera(),
+                child: Text('Click Here To Select Image From Camera'),
+                textColor: Colors.white,
+                color: Colors.green,
+                padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
+              )),
+          Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+              child: RaisedButton(
+                onPressed: () => getImageFromGallery(),
+                child: Text('Click Here To Select Image From Gallery'),
+                textColor: Colors.white,
+                color: Colors.green,
+                padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
+              ))
+        ])));
   }
 }
